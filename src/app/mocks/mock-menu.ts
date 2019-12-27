8b97556d37cac1fa34983e5317cd64e79af30823
fabio.franchi <fabio.franchi@dataprev.gov.br>
2019-12-27 11:56:50 -0300

    Ajustando artigos

--- src/app/mocks/mock-menu.ts
+++ src/app/mocks/mock-menu.ts
@@ -4,6 +4,5 @@ export const MENUS: Menu[] = [
     { id: 1, texto: 'Home', menuPai: undefined },
     { id: 2, texto: 'Identificar', menuPai: undefined },
     { id: 3, texto: 'Notícias', menuPai: undefined },
-    { id: 4, texto: 'Denuncie', menuPai: undefined },
-    { id: 5, texto: 'Contato', menuPai: undefined }
+    { id: 4, texto: 'Contato', menuPai: undefined }
 ];
